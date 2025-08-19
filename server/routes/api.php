f031a5133c3cd6d992fcbaaf0c6083e77e6485fe
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-08-19 23:50:00 +0800

    Refactor API routes to use namespaced ProjectIdeaController and update route paths

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProjectIdeaController;
+use App\Http\Controllers\Api\ProjectIdeaController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/generate-project', [ProjectIdeaController::class, 'generate']);
 
+// ? Project Ideas API routes
+Route::post('generate-ideas', [ProjectIdeaController::class, 'generate']);
+Route::get('options', [ProjectIdeaController::class, 'options']);
