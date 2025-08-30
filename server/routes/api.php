e0e5e12d747ee425ca301694792e253201dd4a46
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-08-31 01:45:17 +0800

    feat: add authentication routes for register, login, and logout in API

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,23 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectIdeaController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 // ? Project Ideas API routes
 Route::post('generate-ideas', [ProjectIdeaController::class, 'generate']);
 Route::get('options', [ProjectIdeaController::class, 'options']);
+
+// ? Auths
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+   Route::post('/logout', [AuthController::class, 'logout']);
+});
